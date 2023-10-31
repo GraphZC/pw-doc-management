7aac36de59db35d0d896813108488959756678f8
Tanaroeg O-Charoen <tanaroeg.o@ku.th>
2023-10-31 22:38:57 +0700

    Fix cors

--- src/main/java/com/poolworldpattaya/docmanagement/DocManagementApplication.java
+++ src/main/java/com/poolworldpattaya/docmanagement/DocManagementApplication.java
@@ -2,11 +2,27 @@ package com.poolworldpattaya.docmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DocManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DocManagementApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
